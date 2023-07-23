2cf2ba348c2600c79c69ab8315e526b9d9725125
GT037 <53064476+GodSlayergt@users.noreply.github.com>
2023-07-23 19:14:35 +0000

    Update 4 files

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -1,70 +1,60 @@
 import io from "socket.io/node_modules/socket.io-client/dist/socket.io";
 import store from "./store";
 
+const socket = io(process.env.REACT_APP_SERVER_URL);
+const sock = {};
+socket.on("connect", () => {
+  console.log("connected");
+});
 
+socket.on("disconnect", () => {
+  console.log("dissconnected");
+});
 
-const socket=io("http://127.0.0.1:3001/");
-const sock={};
-socket.on("connect",()=>{
-    console.log("connected");
-    
-})
+socket.on("messg", (data) => {
+  console.log(data);
+  store.dispatch({ type: "add", data: data });
+});
 
-socket.on("disconnect",()=>{
-    console.log("dissconnected");
-})
+socket.on("notification", (data) => {
+  store.dispatch({ type: "req", req: data });
+  alert("you have a friend request from " + data.name);
+});
 
+socket.on("reqaccepted", (data) => {
+  alert("your request has been accepted By " + data.name);
+});
 
-socket.on("messg",(data)=>{
-    console.log(data);
-    store.dispatch({type:"add",data:data});
-})
+socket.on("totalonline", (data) => {
+  console.log("online users");
+  console.log(data);
+  store.dispatch({ type: "onlineusers", onlineusers: data });
+});
 
-socket.on("notification",(data)=>{
-    store.dispatch({type:"req",req:data});
-    alert("you have a friend request from " + data.name);
-})
+socket.on("googelres", (data) => {
+  console.log("googleres");
+  console.log(data);
+  store.dispatch({ type: "user", onlineusers: data });
+});
 
-socket.on("reqaccepted",(data)=>{
-    
-    alert("your request has been accepted By "+ data.name);
-})
+sock.send = (data) => {
+  socket.emit("messg", data);
+};
 
-socket.on("totalonline",(data)=>{
-    console.log("online users");
-    console.log(data);
-    store.dispatch({type:"onlineusers",onlineusers:data});
-})
+sock.sendreq = (data) => {
+  socket.emit("sendreq", data, () => {
+    console.log("request sended");
+  });
+};
 
-socket.on("googelres",(data)=>{
-    console.log("googleres");
-    console.log(data);
-    store.dispatch({type:"user",onlineusers:data});
-})
+sock.acceptreq = (data) => {
+  socket.emit("acceptreq", data);
+};
 
-
-sock.send=(data)=>{
-    
-    socket.emit("messg",data);
-}
-
-sock.sendreq=(data)=>{
-    socket.emit("sendreq",data,()=>{
-        console.log("request sended");
-    })
-}
-
-sock.acceptreq=(data)=>{
-    socket.emit("acceptreq",data);
-}
-
-
-
-sock.join=(data)=>{
-    socket.emit("join",data,()=>{
-        console.log("joined");
-    });
-}
-
+sock.join = (data) => {
+  socket.emit("join", data, () => {
+    console.log("joined");
+  });
+};
 
 export default sock;
